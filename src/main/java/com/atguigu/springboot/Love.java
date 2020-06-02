005cd3958e9dfea55363c6e54ab4adea5db5bf21
远赴惊鸿 <atguigu.com>
2020-06-02 21:40:03 +0800

    新建Love.java

---
+++ src/main/java/com/atguigu/springboot/Love.java
@@ -0,0 +1,11 @@
+package com.atguigu.springboot;
+
+/**
+ * @author Administrator
+ * @create 2020-06-02 21:38
+ */
+public class Love {
+    public static void main(String[] args) {
+        System.out.println("我爱段淑娜");
+    }
+}
